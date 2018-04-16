9ff1edbe90426adc7ff40e4f0b358399a573c026
luckboy心 <1062329969@qq.com>
2018-04-16 14:32:36 +0800

    提交

---
+++ models/user.js
@@ -0,0 +1,29 @@
+/**
+ * Created by jun on 2016/4/23.
+ * 用户模型
+ */
+var mysql = require('mysql');
+var dbconfig = require("../config/database");
+
+//使用连接池
+var pool = mysql.createPool(dbconfig.mysql);
+
+module.exports = {
+    showUser : function(req,res,callback) {
+        pool.getConnection(function(err, connection) {
+            //定义查询语句
+            var sql = "SELECT * FROM `test`";
+            // console.log(sql);
+            connection.query(sql,function(err,result) {
+                console.log(result);
+                result = JSON.stringify(result);
+                callback(err,result);
+                // 释放连接
+                connection.release();
+            })
+        });
+    },
+    test:function() {
+        return "hello world from test";
+    }
+}
